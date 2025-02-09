e78f661be21b1e6cafb29598ba088e396ccdc6b4
Helen1553 <alena.tyulinaa@gmail.com>
2025-02-09 23:51:54 +0300

    попытка сделать task3

--- task2/assets/scripts/index.js
+++ task2/assets/scripts/index.js
@@ -1,3 +1,11 @@
+const avatars = [
+	'../images/img_cat1.jpg',
+	'../images/img_cat2.jpg',
+	'../images/img_cat3.jpg',
+	'../images/img_dog1.jpg',
+	'../images/img_dog2.jpg'
+];
+
 const addComment = () => {
 	// Запишем в переменную текущее значение инпута (а для начала найдём все необходимые элементы в html)
 	const name = document.getElementById("name-input").value
@@ -10,6 +18,16 @@ const addComment = () => {
     //Приводим первую букву имени к верхнему регистру, а остальные буквы — к нижнему
 	const name3 = name2.charAt(0).toUpperCase() + name2.slice(1).toLowerCase();
 
+	const showName = document.getElementById("show-name-checkbox").checked;
+
+	// // Заменяет на username, если имя не введено
+	// const username = "username";
+	// const nameToDisplay = (showName && nameInput) ? nameInput : username;
+	
+	// Получаем текущую дату и время
+	const date = new Date();
+	const dateString = date.toLocaleString();
+
 
 	//Создаём массив возможных плохих словечек от пользователя
 	const badWords = ['shit', 'viagra', 'damn'];
@@ -20,22 +38,34 @@ const addComment = () => {
 
 
     // Создаём новый элемент - <div>, содержащий текст отформатированного имени
-	const divChat = document.createElement("div");
-	divChat.textContent = name3;
-	divChat.classList.add("name");
-	chat.appendChild(divChat);
+	const userName = document.createElement("div");
+	userName.textContent = name3;
+	userName.classList.add("name");
+	chat.appendChild(userName);
 
     // Создаём еще один элемент <div>, содержащий аватарку
-	const div2Chat = document.createElement("img");
-	div2Chat.src = link;
-	div2Chat.classList.add("image");
-	chat.appendChild(div2Chat);
+	const userImage = document.createElement("img");
+	userImage.src = link;
+	userImage.classList.add("image");
+	chat.appendChild(userImage);
 
 	// Создаём еще один элемент <div>, содержащий сам комментарий
-	const div3Chat = document.createElement("div");
-	div3Chat.textContent = commentReplace;
-	div3Chat.classList.add("comment");
-	chat.appendChild(div3Chat);
+	const userComment = document.createElement("div");
+	userComment.textContent = commentReplace;
+	userComment.classList.add("comment");
+	chat.appendChild(userComment);
+
+
+	userComment.innerHTML = 
+    `<div>
+        <img src="${avatar}" alt="Аватар" style="width: 50px; height: 50px;">
+        <strong>${nameToDisplay}</strong> 
+        <span style="font-size: small;">(${dateString})</span>
+    </div>
+    <div>${comment}</div>
+	`;
+
+chat.appendChild(userComment);
 
 	//Код ниже реализует очистку всех полей ввода, чтобы подготовить их для следующего ввода
 	document.getElementById("name-input").value = "";
